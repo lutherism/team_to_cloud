b24f24826100a24b154929cdeceec8d886aa76cd
lutherism <xanderjanz@alexander-jansens-macbook-2.local>
2014-09-29 16:27:53 -0700

    work

--- scripts/App.js
+++ scripts/App.js
@@ -5,31 +5,54 @@ define([
 	'views/WikiCloudCanvas'
 	], function($, _, Backbone, WikiCloudCanvas) {
 	var App = Backbone.View.extend({
+		events: {
+			'click button.fullBan': 'banWords'
+		},
 		initialize: function(options) {
 			this.teamData = options.teamData;
 			this.buildClouds();
 			App.__super__.initialize.apply(this, arguments);
 		},
 		render: function() {
-			this.$el.html('<div class="app"></div>');
-			for (var i in this.teamData['NFL']) {
-				this.clouds[i].maybeRender();
-				this.$el.append(this.clouds[i].el);
-			}
+			this.$el.html('<div class="app"><textarea placeholder="seperate words with , "></textarea><button class="fullBan">Ban Words</button></div>');
+			//for (var league in this.teamData) {
+			var league = 'NFL';
+				for (var team in this.teamData[league]) {
+				this.clouds[team].maybeRender();
+				this.$el.append(this.clouds[team].el);
+			}//}
 			App.__super__.render.apply(this, arguments);
 			return this;
 		},
+		banWords: function(e) {
+			var val = this.$el.find('textarea').val();
+			var words = val.split(", ");
+			//for (var league in this.teamData) {
+			var league = 'NFL';
+				for (var i in this.teamData[league]) {
+				this.clouds[i].banWords(words);
+			}//}
+		},
 		buildClouds: function() {
 			this.clouds = this.clouds || {};
-			for (var team in this.teamData['NFL']) {
-				var colors = this.teamData['NFL'][team];
-				var view = new WikiCloudCanvas({
-					team: team,
-					colors: colors,
-					size: 600
-				});
-				this.clouds[team] = view;
-			}
+			//for (var league in this.teamData) {
+			var league = 'NFL';
+				for (var team in this.teamData[league]) {
+					var colors = this.teamData[league][team];
+					var view = new WikiCloudCanvas({
+						team: team,
+						colors: colors,
+						sizeX: 400,
+						sizeY: 700,
+						words: 250,
+						bannedWords: ['and','the','to','a','of','for','as','i','with','it',
+							'is','on','that','this','can','in','be','has','if', '^', '-',
+							'by', 'i', '0', 'lost', 'they', 'their', 'at', '•', '–', 'who',
+							't', 'from', 'was', '[am]', '1', 'he', 'pants', 'after']
+					});
+					this.clouds[team] = view;
+				}
+			//}
 		}
 	});
 	return App;
--- scripts/views/WikiCloudCanvas.js
+++ scripts/views/WikiCloudCanvas.js
@@ -4,12 +4,18 @@ define([
 	'backbone',
 	'd3-cloud'
 	], function($, _, Backbone, d3) {
-		var WikiCloudCanvas = Backbone.View.extend( {
+		var WikiCloudCanvas = Backbone.View.extend({
+			events: {
+				'click .colors': 'tryNewColors',
+				'click .respin': 'render',
+				'click .png': 'downloadPNG',
+				'keydown input': 'wordEnter'
+			},
 			draw: function(input) {
-			  d3.layout.cloud().size([this.size, this.size])
+			  this.cloudObj = d3.layout.cloud().size([this.sizeX, this.sizeY])
 			      .words(this.wordObjects)
 			      .padding(2)
-			      .rotate(function() { return ~~(Math.random() * 2) * 90; })
+			      .rotate(function() { return (Math.random() * 5) * 90 - 45; })
 			      .font("Impact")
 			      .fontSize(function(d) { return d.size; })
 			      .on("end", _.bind(this.layout, this))
@@ -25,10 +31,10 @@ define([
 				var colors = this.colors;
 				var lastcolor = 0;
 				d3.select('.cloud'+this.cid, this.$el).append("svg")
-					.attr("width", this.size)
-					.attr("height", this.size)
+					.attr("width", this.sizeX)
+					.attr("height", this.sizeY)
 				.append("g")
-					.attr("transform", "translate("+(this.size/2)+","+(this.size/2)+")")
+					.attr("transform", "translate("+(this.sizeX/2)+","+(this.sizeY/2)+")")
 				.selectAll("text")
 					.data(words)
 				.enter().append("text")
@@ -41,19 +47,65 @@ define([
 					})
 					.text(function(d) { return d.text; });
 			},
+			downloadPNG: function(scale) {
+				scale = 2;
+				var fill = _.bind(function() {
+					if (this.lastcolor++ == this.colors.length) {
+						this.lastcolor = 0;
+					}
+					return this.colors[this.lastcolor];
+				}, this)
+				var colors = this.colors;
+				this.lastcolor = 0;
+				 var canvas = document.createElement("canvas");
+				 var c = canvas.getContext("2d");
+				  canvas.width = parseInt(this.sizeX * scale);
+				  canvas.height = parseInt(this.sizeY * scale);
+				  c.translate(parseInt(this.sizeX * (scale / 2)), parseInt(this.sizeY * (scale / 2)));
+				  c.scale(scale, scale);
+				  this.cloudObj.words().forEach(function(word, i) {
+				    c.save();
+				    c.translate(word.x, word.y);
+				    c.rotate(word.rotate * Math.PI / 180);
+				    c.textAlign = "center";
+				    c.fillStyle = '#'+fill();
+				    c.font = word.size + "px " + word.font;
+				    c.fillText(word.text, 0, 0);
+				    c.restore();
+				  });
+				  window.open(canvas.toDataURL("image/png"));
+			},
+			wordEnter: function(e) {
+				if (event.which == 13 || event.keyCode == 13) {
+					this.banWords([$(e.currentTarget).val()])
+				}
+			},
 			initialize: function(options) {
 				WikiCloudCanvas.__super__.initialize.apply(this, arguments);
-				this.colors = options.colors || ["000080", "FF0000", "C0C0C0"];
+				this.colorOptions = options.colors;
+				this.mixColors();
 				this.team = options.team;
-				this.size = options.size;
+				this.sizeX = options.sizeX || 300;
+				this.sizeY = options.sizeY || 300;
+				this.words = options.words || 100;
+				this.bannedWords = options.bannedWords;
 				this.findPageId(this.team);
 			},
+			mixColors: function() {
+				var bg = parseInt(Math.random() * this.colorOptions.length);
+				this.colors = [].concat(this.colorOptions) || ["000080", "FF0000", "C0C0C0"];
+				this.bgColor = this.colorOptions[bg];
+				delete this.colors[bg];
+				this.colors.sort();
+			},
+			tryNewColors: function() {
+				this.mixColors();
+				this.render();
+			},
 			render: function() {
-				this.$el.html('<div class="cloud'+this.cid+'""></div><button>Respin</button>');
-				this.$el.attr('style', 'display:inline-block;');
-				this.$el.find('button').on('click',_.bind(function() {
-					this.render();
-				}, this));
+				this.$el.html('<div class="cloud'+this.cid+'""></div><button class="png">.png</button><input type="text"><button class="colors">New Colors</button><button class="respin">Respin</button>');
+				this.$el.attr('style', 'display:inline-block; background-color:'+this.bgColor);
+				this.$el.prepend('<span style="background-color:black; color:white">' + this.bgColor + '</span>');
 				if (this.wordObjects) {
 					this.draw(this.wordObjects);
 				}
@@ -79,7 +131,7 @@ define([
 				$.ajax({
 					url: "http://en.wikipedia.org/w/api.php?format=json&action=parse&pageid=" + this.pageId + "&prop=text&contentmodel=text",
 					success: _.bind(function(data) {
-						this.bodyText = data.parse.text['*'].replace(/<(?:.|\n)*?>/gm, '');
+						this.bodyText = data.parse.text['*'].replace(/<(?:.|\n)*?>/gm, '').replace(/\{{(?:.|\n)*?}}/gm, '');
 						this.getWordCount(this.bodyText);
 					}, this)
 				});
@@ -87,12 +139,16 @@ define([
 			maybeRender: function() {
 				return this.render();
 			},
+			banWords: function(words) {
+				this.bannedWords = this.bannedWords.concat(words);
+				this.getWordCount(this.bodyText);
+			},
 			getWordCount: function(text) {
 				var sWords = text.split(/[\s\/]+/g).sort();
 				var iWordsCount = sWords.length; // count w/ duplicates
 			 
 				// array of words to ignore
-				var ignore = ['and','the','to','a','of','for','as','i','with','it','is','on','that','this','can','in','be','has','if', '^', '-'];
+				var ignore = this.bannedWords;
 				ignore = (function(){
 					var o = {}; // object prop checking > in array checking
 					var iCount = ignore.length;
@@ -105,17 +161,23 @@ define([
 				var counts = {}; // object for math
 				for (var i=0; i<iWordsCount; i++) {
 					var sWord = sWords[i];
-					if (!ignore[sWord]) {
-						counts[sWord] = counts[sWord] || 0;
-						counts[sWord]++;
+					slWord = sWord.toLowerCase();
+					if (!ignore[slWord]) {
+						counts[slWord] = counts[slWord] || {frequency:0};
+						if (slWord == sWord) {
+							counts[slWord].text = counts[slWord].text || sWord;
+						} else {
+							counts[slWord].text = sWord;
+						}
+						counts[slWord].frequency++;
 					}
 				}
 			 
 				var arr = []; // an array of objects to return
 				for (sWord in counts) {
 					arr.push({
-						text: sWord,
-						frequency: counts[sWord]
+						text: counts[sWord].text,
+						frequency: counts[sWord].frequency
 					});
 				}
 			 
@@ -123,9 +185,9 @@ define([
 				var words = arr.sort(function(a,b){
 					return (a.frequency > b.frequency) ? -1 : ((a.frequency < b.frequency) ? 1 : 0);
 				});
-				this.wordObjects = words.slice(0, 50)
+				this.wordObjects = words.slice(0, this.words)
 				this.wordObjects = this.wordObjects.map(_.bind(function(d) {
-			        return {text: d.text, size: 10 + (d.frequency / this.wordObjects[0].frequency) * 90};
+			        return {text: d.text, size: 8 + (d.frequency / this.wordObjects[0].frequency) * 72};
 			      }, this));
 				this.render();
 			}
